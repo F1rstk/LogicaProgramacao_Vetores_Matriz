6f0be5ef936bf5f3bf993f89836fdef9bce04eed
Filipe <filipe.sdiniz@senacsp.edu.br>
2024-06-20 14:12:52 -0300

    Adcionando Matriz.Ex4

---
+++ MatrizEx4.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Digite o valor de M (número de linhas): ");
+int M = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o valor de N (número de colunas): ");
+int N = int.Parse(Console.ReadLine());
+
+
+double[,] matriz = new double[M, N];
+
+
+for (int i = 0; i < M; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        Console.Write($"Digite o elemento da posição [{i}, {j}]: ");
+        matriz[i, j] = double.Parse(Console.ReadLine());
+    }
+}
+for (int i = 0; i < N; i++) 
+{
+    for (int j = 0; j < N; j++)
+    {
+        if (matriz[i, j] < 0)
+        {
+            Console.WriteLine($"Posição[{i},{j}]: {matriz[i, j]} ");
+        }
+    }
+}
